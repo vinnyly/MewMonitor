8d74943d87c4712c477d8cd89dfe0570fb5f20c3
vinnyly <vincently06@gmail.com>
2025-12-10 12:15:40 -0800

    added stuff to work for deploying to vercel

--- api/index.js
+++ api/index.js
@@ -62,10 +62,12 @@ app.get('/', (req, res) => {
 
 
 
-
-
-
 // START SERVER
-app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
-});
+// Only run app.listen if we are running locally (not on Vercel)
+if (require.main === module) {
+    app.listen(PORT, () => {
+        console.log(`Server running on http://localhost:${PORT}`);
+    });
+}
+// Required for Vercel to turn this into a serverless function
+module.exports = app;
